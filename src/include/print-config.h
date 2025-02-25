a604077c5d619b4267ba0b8df32e9136f57e9929
John Kaul <john.kaul@outlook.com>
2025-02-24 19:28:00 -0600

    print-config.h:
    1. Added function headers.
    2. Added two extra function defs: `add_to_array` & `assemble_strings`
       which should allow me to start coding the "+=" feature.

--- src/include/print-config.h
+++ src/include/print-config.h
@@ -1,5 +1,25 @@
-/* This file was automatically generated.  Do not edit! */
-#undef INTERFACE
+/**
+ * This code will print a value string (array) to a config file.
+ *
+ * These functions accpet an array and locate the first value in the
+ * array in the config file and replace the config value with the
+ * given array contents.
+ */
 
+//: replacevariable
+//      Replaces a items value in the config file.
 int replacevariable(const char *key, char **value, int count, const char *filename);
+
+//: Printconfifile
+//      Iterates the `config_array` and prints the items.
 void printconfigfile(config_t *config_array,int array_count);
+
+//: assemble_strings
+//      This function assebles the array of char arrays into a string
+//      (ommiting the first char array which should be the 'key' in a
+//      key/value string).
+char* assemble_strings(char **value, int count);
+
+//: add_to_array
+//      This function will append a string to given `argvp`.
+int add_to_array(char ***argvp, int size, const char *string);
